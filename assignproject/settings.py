39824763689983d5fb34804c2734e1cf66bc085b
Chandan746 <Chandansinha007@outlook.com>
2020-07-07 15:45:56 +0530

    first

--- assignproject/settings.py
+++ assignproject/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '#d-^clb)7d$or2t&186le0uol*dzb(q(h8#0&93vl4rm7l*@ul'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -108,5 +108,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
